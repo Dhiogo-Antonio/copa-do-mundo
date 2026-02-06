e27572e6d85c9017ddbc5e998e7dd2bb4e62278a
Dhiogo-Antonio <dhiogo.marestoni@gmail.com>
2026-02-06 14:28:33 -0300

    cadastro

---
+++ view/cadastro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <form method="post">
+
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" required><br>
+
+    <label for="nome">Idade:</label>
+    <input type="text" name="nome" required><br>
+
+    <label for="nome">Seleção epresentante:</label>
+    <input type="text" name="nome" required><br>
+
+    <label for="nome">Cargo:</label>
+    <input type="text" name="nome" required><br>
+
+    <label for="email">E-mail:</label>
+    <input type="text" name="email" required><br>
+
+    <label for="senha">Senha:</label>
+    <input type="password" name="senha" required><br>
+
+    <input type="submit">
+
+    </form>
+    </div>
+</body>
+</html>
