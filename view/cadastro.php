5156e1d03a50ccfebb0f8855706c9c533fbef6f0
Dhiogo-Antonio <dhiogo.marestoni@gmail.com>
2026-02-06 14:48:27 -0300

    cad

--- view/cadastro.php
+++ view/cadastro.php
@@ -12,14 +12,19 @@
     <label for="nome">Nome:</label>
     <input type="text" name="nome" required><br>
 
-    <label for="nome">Idade:</label>
-    <input type="text" name="nome" required><br>
+    <label for="idade">Idade:</label>
+    <input type="number" name="idade" required><br>
 
-    <label for="nome">Seleção epresentante:</label>
-    <input type="text" name="nome" required><br>
+    <label for="selecao">Seleção epresentante:</label>
+    <input type="text" name="selecao" required><br>
 
-    <label for="nome">Cargo:</label>
-    <input type="text" name="nome" required><br>
+    <select name="cargo" id="cargo">
+     <option value="">Cargo</option>
+     <option value="">Jogador</option>
+     <option value="">Técnico</option>
+     <option value="">Arbito</option>
+     <option value="">Bandeira</option>
+    </select>
 
     <label for="email">E-mail:</label>
     <input type="text" name="email" required><br>
